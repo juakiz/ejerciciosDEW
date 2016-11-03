c34e47d18251c6f3c0148514428406baaecc3428
Unknown <juakiz@gmail.com>
2016-11-03 17:58:43 +0000

    20161103

---
+++ codigo.js
@@ -0,0 +1,25 @@
+//funcion que escribe mi "primer" script segun el parámetro introducido
+function hola (int str)
+	    {
+	    	switch(str)
+	    	{
+	    		case 1:
+	    			alert("Hola mundo");
+	    		break;
+
+	    		case 2:
+	    			alert("Hola Carlos");
+	    		break;
+
+	    		case 3:
+	    			alert("Adios mundo");
+	    		break;
+
+				case 4:
+	    			alert("Adios Carlos");
+	    		break;
+	    		default:
+	    			alert("No encuentro la opción");
+
+	    	}
+	    }
